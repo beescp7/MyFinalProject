5f428e9cc122d6f6524ae5b4418fd4ab6afdc974
berkan olgun <bolgun860@gmail.com>
2022-01-30 22:42:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Business.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager; 
+
+            Console.WriteLine();
+        }
+    }
+}
